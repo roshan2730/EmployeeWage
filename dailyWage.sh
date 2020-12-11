eb783778ac7384ea7031e780d3b7bb19693578be
Roshan Gupta <roshangupta@Roshans-MacBook-Air.local>
2020-12-11 13:58:29 +0530

    Daily wage code added

---
+++ dailyWage.sh
@@ -0,0 +1,17 @@
+#!/bin/bash -x
+echo Welcome To Employee Wage problem.
+
+#usecase being solved showing employee is present or not.
+#Declaring Varriables.
+isPresent=1
+attendanceCheck=$((RANDOM%2))
+wagePerHour=20
+#Logic for checking employee is present or not.
+if (( attendanceCheck == isPresent ))
+then
+	totalWage=$((wagePerHour*8))
+	echo "Employee is present and his daily wage is $totalWage"
+else
+	echo Employee is Absent
+fi
+
